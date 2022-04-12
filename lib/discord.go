c6bdce55b57f6f15c567b96160fa7147ded089c9
Germano Eichenberg <germanoeich@gmail.com>
2022-04-12 20:55:59 -0300

    Disable http2, take 2

--- lib/discord.go
+++ lib/discord.go
@@ -31,7 +31,20 @@ type BotUserResponse struct {
 
 func createTransport(ip string) http.RoundTripper {
 	if ip == "" {
-		return http.DefaultTransport
+		// http.DefaultTransport options, with http2 disabled
+		return &http.Transport{
+			Proxy: http.ProxyFromEnvironment,
+			DialContext: (&net.Dialer{
+				Timeout:   30 * time.Second,
+				KeepAlive: 30 * time.Second,
+			}).DialContext,
+			ForceAttemptHTTP2:     false,
+			MaxIdleConns:          100,
+			IdleConnTimeout:       90 * time.Second,
+			TLSHandshakeTimeout:   10 * time.Second,
+			ExpectContinueTimeout: 1 * time.Second,
+			TLSNextProto: map[string]func(string, *tls.Conn) http.RoundTripper{},
+		}
 	}
 	addr, err := net.ResolveTCPAddr("tcp", ip+":0")
 
@@ -55,7 +68,7 @@ func createTransport(ip string) http.RoundTripper {
 	}
 
 	transport := http.Transport{
-		ForceAttemptHTTP2:     true,
+		ForceAttemptHTTP2:     false,
 		MaxIdleConns:          1000,
 		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
