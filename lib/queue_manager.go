c67b3c7707aa463ea28e4e03fc801d14ab65d375
Germano Eichenberg <germanoeich@gmail.com>
2022-05-19 17:27:34 -0300

    Fix logEntry reassignment

--- lib/queue_manager.go
+++ lib/queue_manager.go
@@ -334,7 +334,7 @@ func (m *QueueManager) fulfillRequest(resp *http.ResponseWriter, req *http.Reque
 				logEntry.WithField("function", "CopyResponseToResponseWriter").Error(err)
 			}
 		} else {
-			logEntry := logEntry.WithField("function", "routeRequest")
+			logEntry = logEntry.WithField("function", "routeRequest")
 			if !errors.Is(err, context.Canceled) {
 				logEntry.Error(err)
 			} else {
